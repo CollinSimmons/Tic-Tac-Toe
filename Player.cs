f3a4730e6e72ea90d087268ace2d50a18ead0572
collinsimmons <collinsimmons80@gmail.com>
2023-11-14 16:31:37 -0600

    added the player class

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+namespace Practice
+{
+	public class Player
+	{
+
+
+            Element element;
+            Element op;
+
+
+            public Player(Element el, Element opposition)
+            {
+                element = el;
+                op = opposition;
+            }
+
+            public void Play(Board board)
+            {
+
+                var a = Console.ReadLine();
+                if (a == "topleft")
+                {
+                    //Adding comment to improve something
+                    if (board.grid[0, 0] == op)
+                    {
+                        Console.WriteLine("Not an empty space");
+                    }
+                    else
+                    {
+                        board.grid[0, 0] = element;
+                    }
+                }
+            }
+        }
+    
+	
+}
+
